ce1f1dc075fa984c8137a4ef7dae24a7f26cd0c8
Abishalini <abi.gpuram@gmail.com>
2022-10-18 16:39:51 -0600

    Fix indentation

--- robot_calibration/src/util/action_server.cpp
+++ robot_calibration/src/util/action_server.cpp
@@ -20,14 +20,14 @@ CalibratePoseServer::CalibratePoseServer(const rclcpp::Node::SharedPtr& node, st
       std::bind(&CalibratePoseServer::handle_cancel, this, _1),
       std::bind(&CalibratePoseServer::handle_accepted, this, _1));
 
-	load_robot_poses();
+  load_robot_poses();
 
   capture_manager.init(node_);
 }
 
 void CalibratePoseServer::load_robot_poses()
 {
-	RCLCPP_INFO(node_->get_logger(), "Loading YAML calibration poses from %s", robot_poses_path_.c_str());
+  RCLCPP_INFO(node_->get_logger(), "Loading YAML calibration poses from %s", robot_poses_path_.c_str());
     if (!robot_calibration::getPosesFromYaml(robot_poses_path_, robot_poses))
     {
       // Error will be printed in function
@@ -36,20 +36,20 @@ void CalibratePoseServer::load_robot_poses()
 }
 
 rclcpp_action::GoalResponse CalibratePoseServer::handle_goal(
-	const rclcpp_action::GoalUUID & uuid,
-	std::shared_ptr<const CalibratePose::Goal> goal)
+  const rclcpp_action::GoalUUID & uuid,
+  std::shared_ptr<const CalibratePose::Goal> goal)
 {
-	RCLCPP_INFO(node_->get_logger(), "Received goal request");
-	(void)uuid;
-	return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+  RCLCPP_INFO(node_->get_logger(), "Received goal request");
+  (void)uuid;
+  return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
 }
 
 rclcpp_action::CancelResponse CalibratePoseServer::handle_cancel(
-	const std::shared_ptr<GoalHandleCalibratePose> goal_handle)
+  const std::shared_ptr<GoalHandleCalibratePose> goal_handle)
 {
-	RCLCPP_INFO(node_->get_logger(), "Received request to cancel goal");
-	(void)goal_handle;
-	return rclcpp_action::CancelResponse::ACCEPT;
+  RCLCPP_INFO(node_->get_logger(), "Received request to cancel goal");
+  (void)goal_handle;
+  return rclcpp_action::CancelResponse::ACCEPT;
 }
 
 void CalibratePoseServer::handle_accepted(const std::shared_ptr<GoalHandleCalibratePose> goal_handle)
@@ -58,13 +58,13 @@ void CalibratePoseServer::handle_accepted(const std::shared_ptr<GoalHandleCalibr
 }
 
 void CalibratePoseServer::execute(const std::shared_ptr<GoalHandleCalibratePose> goal_handle) {
-	auto logger = node_->get_logger();
-	const auto goal = goal_handle->get_goal();
+  auto logger = node_->get_logger();
+  const auto goal = goal_handle->get_goal();
 
-	// Save URDF for calibration/export step
-	description_msg.data = capture_manager.getUrdf();
+  // Save URDF for calibration/export step
+  description_msg.data = capture_manager.getUrdf();
 
-	// Loop through robot poses if read from YAML or bag file
+  // Loop through robot poses if read from YAML or bag file
   if (!robot_poses.empty()) 
   {
     for (unsigned pose_idx = 0; pose_idx < robot_poses.size() && rclcpp::ok();
@@ -98,7 +98,7 @@ void CalibratePoseServer::execute(const std::shared_ptr<GoalHandleCalibratePose>
     RCLCPP_INFO(logger, "Done capturing samples");
   }
 
-	// Create instance of optimizer
+  // Create instance of optimizer
   robot_calibration::OptimizationParams params;
   robot_calibration::Optimizer opt(description_msg.data);
 
@@ -113,43 +113,43 @@ void CalibratePoseServer::execute(const std::shared_ptr<GoalHandleCalibratePose>
   // Run calibration steps
   for (auto step : calibration_steps)
   {
-		params.LoadFromROS(node_, step);
+    params.LoadFromROS(node_, step);
 
-		// If we receive initial guess from action goal, override 
-		std::string initial_guess_param = step + ".free_frames_initial_values";
-		RCLCPP_INFO(logger, "Overriding initial values for: %s", initial_guess_param.c_str());
+    // If we receive initial guess from action goal, override 
+    std::string initial_guess_param = step + ".free_frames_initial_values";
+    RCLCPP_INFO(logger, "Overriding initial values for: %s", initial_guess_param.c_str());
 
-		for (unsigned i=0; i < params.free_frames.size(); i++) {
-			for (const auto& estimated_pose : goal->estimated_poses) {
-				if (estimated_pose.child_frame_id == params.free_params.at(i)) {
-					params.free_frames_initial_values.at(i).x = estimated_pose.transform.translation.x;
-					params.free_frames_initial_values.at(i).y = estimated_pose.transform.translation.y;
-					params.free_frames_initial_values.at(i).z = estimated_pose.transform.translation.z;
+    for (unsigned i=0; i < params.free_frames.size(); i++) {
+      for (const auto& estimated_pose : goal->estimated_poses) {
+        if (estimated_pose.child_frame_id == params.free_params.at(i)) {
+          params.free_frames_initial_values.at(i).x = estimated_pose.transform.translation.x;
+          params.free_frames_initial_values.at(i).y = estimated_pose.transform.translation.y;
+          params.free_frames_initial_values.at(i).z = estimated_pose.transform.translation.z;
 
-					tf2::Quaternion q(estimated_pose.transform.rotation.x,
-														estimated_pose.transform.rotation.y,
-														estimated_pose.transform.rotation.z,
-														estimated_pose.transform.rotation.w);
-					tf2::Matrix3x3 m(q);
-					double roll, pitch, yaw;
-					m.getRPY(roll, pitch, yaw);
+          tf2::Quaternion q(estimated_pose.transform.rotation.x,
+                            estimated_pose.transform.rotation.y,
+                            estimated_pose.transform.rotation.z,
+                            estimated_pose.transform.rotation.w);
+          tf2::Matrix3x3 m(q);
+          double roll, pitch, yaw;
+          m.getRPY(roll, pitch, yaw);
 
-					params.free_frames_initial_values[i].roll = roll;
-					params.free_frames_initial_values[i].pitch = pitch;
-					params.free_frames_initial_values[i].yaw = yaw;
-				}
-			}
-		}
+          params.free_frames_initial_values[i].roll = roll;
+          params.free_frames_initial_values[i].pitch = pitch;
+          params.free_frames_initial_values[i].yaw = yaw;
+        }
+      }
+    }
 
     opt.optimize(params, data, logger, true);
 
-		std::cout << "Parameter Offsets:" << std::endl;
-		std::cout << opt.getOffsets()->getOffsetYAML() << std::endl;
+    std::cout << "Parameter Offsets:" << std::endl;
+    std::cout << opt.getOffsets()->getOffsetYAML() << std::endl;
   }
 
   // Write outputs
   // robot_calibration::exportResults(opt, description_msg.data, data);
-	auto result = std::make_shared<CalibratePose::Result>();
+  auto result = std::make_shared<CalibratePose::Result>();
 
   RCLCPP_INFO(logger, "Done calibrating");
 }
